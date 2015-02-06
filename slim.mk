d30af3b00caeb3e665a02962dbf1a1f15c66e6b4
Jon Kagan <darkspadez@users.noreply.github.com>
2015-02-06 14:58:07 -0500

    Update and rename cm.mk to slim.mk

---
+++ slim.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := peregrine
+PRODUCT_NAME := slim_peregrine
