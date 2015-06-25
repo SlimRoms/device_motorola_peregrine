937dd6a13e4107a75ee9c925b36c55ef74a7bc3f
Josue Rivera <prbassplayer@gmail.com>
2015-06-25 16:03:39 -0400

    Peregrine: Cleanup slim.mk a little
      * Inherit common bits before specific
    
    Change-Id: Ida202bd0fe483db7d3071c6220ef5fc0cf5b6410
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -1,5 +1,8 @@
-$(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
+# Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device specific
+$(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := slim_peregrine
